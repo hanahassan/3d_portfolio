54bf5a4f31cb566d9adf48f3ffac3fa7fdc079d1
hanahassan <hana0hassan0@gmail.com>
2024-06-26 15:09:27 -0600

    Turns out it didn't help

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import { Home, About, Projects, Contact } from './pages';
+import { Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import {Home, About, Projects, Contact} from './pages';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <main className='bg-slate-300/20 flex flex-col min-h-screen'>
+    <main className='bg-slate-300/20'>
       <Router>
         <Navbar />
-        <div className='flex-grow'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/projects' element={<Projects />} />
-            <Route path='/contact' element={<Contact />} />
-          </Routes>
-        </div>
-        <Footer />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route
+            path='/*'
+            element={
+              <>
+                <Routes>
+                  <Route path='/about' element={<About />} />
+                  <Route path='/projects' element={<Projects />} />
+                  <Route path='/contact' element={<Contact />} />
+                </Routes>
+                <Footer />
+              </>
+            }
+          />
+        </Routes>
       </Router>
     </main>
   );
