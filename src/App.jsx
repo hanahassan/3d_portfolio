6d457d7a610f70d1f297e80bc3daa0f7fe10e923
hanahassan <hana0hassan0@gmail.com>
2024-06-26 15:07:22 -0600

    fixed fox on iphones

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,22 @@
-import { Route, BrowserRouter as Router, Routes} from 'react-router-dom';
-import {Home, About, Projects, Contact} from './pages';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Home, About, Projects, Contact } from './pages';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <main className='bg-slate-300/20'>
+    <main className='bg-slate-300/20 flex flex-col min-h-screen'>
       <Router>
         <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route
-            path='/*'
-            element={
-              <>
-                <Routes>
-                  <Route path='/about' element={<About />} />
-                  <Route path='/projects' element={<Projects />} />
-                  <Route path='/contact' element={<Contact />} />
-                </Routes>
-                <Footer />
-              </>
-            }
-          />
-        </Routes>
+        <div className='flex-grow'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/contact' element={<Contact />} />
+          </Routes>
+        </div>
+        <Footer />
       </Router>
     </main>
   );
